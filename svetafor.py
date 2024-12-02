bdc35d1df7615fcfbfa14d92a5075b6eff4314bb
Akmal <akmaldavlatboyev61@gmail.com>
2024-12-02 14:11:16 +0500

    Update and rename svetafor/main.py to svetafor.py

---
+++ svetafor.py
@@ -0,0 +1,58 @@
+
+# import os
+# import time
+# while True:
+#     os.system("clear")
+#     print("🔴")
+#     print("⚫️")
+#     print("⚫️")
+#
+#     time.sleep(6)
+#     os.system('clear')
+#     print("⚫️")
+#     print("🟡")
+#     print("⚫️")
+#     time.sleep(2.5)
+#
+#     for _ in range(3):
+#         os.system('clear')
+#         print("⚫️")
+#         print("🟡")
+#         print("⚫️")
+#         time.sleep(1 / 2)
+#         os.system('clear')
+#         print("⚫️")
+#         print("⚫️")
+#         print("⚫️")
+#         time.sleep(1 / 2)
+#         os.system('clear')
+#
+#     os.system('clear')
+#     print("⚫️")
+#     print("⚫️")
+#     print("🟢")
+#     time.sleep(6)
+#     os.system('clear')
+#     print("⚫️")
+#     print("🟡")
+#     print("⚫️")
+#     time.sleep(2.5)
+#
+#     for _ in range(3):
+#         os.system('clear')
+#         print("⚫️")
+#         print("🟡")
+#         print("⚫️")
+#         time.sleep(1 / 2)
+#         os.system('clear')
+#         print("⚫️")
+#         print("⚫️")
+#         print("⚫️")
+#         time.sleep(1 / 2)
+#         os.system('clear')
+
+
+
+
+
+
